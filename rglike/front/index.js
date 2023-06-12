72c12a951366c7c8e01746583b4908335a2f4a12
Jackson Morgan <40726010+jckmo@users.noreply.github.com>
2023-06-12 15:36:04 -0400

    Add files via upload

---
+++ rglike/front/index.js
@@ -0,0 +1,64 @@
+let canvas = document.querySelector('#main-canvas')
+let game = document.querySelector('#canvas-wrapper')
+
+let fsButton = document.querySelector('#full-screen')
+fsButton.addEventListener('click', () => {
+  game.requestFullscreen()
+})
+
+
+let player = document.querySelector('#player')
+let speed = 10
+
+const checkBounds = (currentPos, downKey) => {
+  // switch to query canvas size
+  let bottomBound = document.querySelector('#main-canvas').clientHeight
+  let rightBound = document.querySelector('#main-canvas').clientWidth
+  let leftBound = 10
+  let topBound = 10
+
+  if (currentPos.top <= topBound) {
+    player.style.top = topBound
+    if (downKey == 'w') {
+      return 0
+    } else return speed
+  } else if (currentPos.left <= leftBound) {
+    player.style.left = leftBound
+    if (downKey == 'a') {
+      return 0
+    } else return speed
+  } else if (currentPos.top >= bottomBound) {
+    player.style.top = bottomBound
+    if (downKey == 's') {
+      return 0
+    } else return speed
+  } else if (currentPos.left >= rightBound) {
+    player.style.left = rightBound
+    if (downKey == 'd') {
+      return 0
+    } else return speed
+  } else return speed
+}
+
+const handleMove = (e) => {
+  currentPos = {top: parseInt(player.style.top), left: parseInt(player.style.left)}
+
+  switch (e.key) {
+    case 'w':
+      player.style.top = currentPos.top - checkBounds(currentPos, e.key) + 'px'
+      break;
+    case 'a':
+      player.style.left = currentPos.left - checkBounds(currentPos, e.key) + 'px'
+      break;
+    case 's':
+      player.style.top = currentPos.top + checkBounds(currentPos, e.key) + 'px'
+      break;
+    case 'd':
+      player.style.left = currentPos.left + checkBounds(currentPos, e.key) + 'px'
+      break;
+    default:
+      break;
+  }
+}
+
+window.addEventListener('keydown', e => handleMove(e))
